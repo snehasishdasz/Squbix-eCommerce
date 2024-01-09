38f7a6bd60c6bd38a0a93459c1ede2a50d880df1
Snehasish Das <78856467+snehasishdasz@users.noreply.github.com>
2024-01-10 03:53:58 +0530

    Product list in homepage Done by fetch api and basic styling done

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Products from './Products';
 
 function Home() {
   return (
     <div className="hero">
       <div className="card bg-dark text-white border-0">
-        <img src="/assets/bg4.jpg" className="card-img" alt="Background" height="660px" />
+        <img src="/assets/bg3.png" className="card-img" alt="Background" height="660px" />
         <div className="card-img-overlay d-flex flex-column justify-content-center">
             <div className="container">
           <h5 className="card-title display-3 fw-bolder mb-0"><span  className='bg-light text-dark px-2' >NEW SEASON</span> ARRIVALS</h5>
@@ -14,6 +15,15 @@ function Home() {
             </div>
         </div>
       </div>
+      <Products/>
+      <footer class=" text-center text-lg-start ">
+  
+  <div class="text-center p-3 ">
+  Created with ❤️ by Snehasish
+    <a  href="https://github.com/snehasishdasz" target='_blank'> Github</a>
+  </div>
+  
+</footer>
     </div>
   );
 }
