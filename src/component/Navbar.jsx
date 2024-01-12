d97ba3ec9f1b37cdffb00a71708053de93b8a62b
Snehasish Das <78856467+snehasishdasz@users.noreply.github.com>
2024-01-12 10:10:49 +0530

    bootstrap hamburger menu problem solved

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
 import { useSelector } from "react-redux";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 function Navbar() {
@@ -13,9 +15,17 @@ function Navbar() {
           <img width="150" height="50" src="/assets/logo2.png" alt="" />
           {/* <span class="text-warning">S</span>D <span>M</span>art */}
           </NavLink>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
